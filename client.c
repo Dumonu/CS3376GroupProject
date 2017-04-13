9ddb3a5a97b2ba5fc740e91b3ddf1b5ba9676219
secondsides <secondsides@gmail.com>
2017-04-13 17:45:01 -0500

    Update client.c

--- client.c
+++ client.c
@@ -1,3 +1,12 @@
+// Program: client.c
+// author: Henry Tran
+// date: 4/13/2017
+// Purpose: CS 3376
+// Description: The steps involved in establishing a socket on the client side are as follows:
+// Create a socket with the socket() system call
+// Connect the socket to the address of the server using the connect() system call
+// Send and receive data. There are a number of ways to do this, but the simplest is to use the read() and write() system calls.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
