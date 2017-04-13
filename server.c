ad41cbc69e771e53b8376d982c9d90f9f8debf9e
IanLlewellyn2 <catluvr11111@gmail.com>
2017-04-13 16:38:20 -0500

    Add files via upload
    added datagram socket and fixed select(). need client.c to test datagram

--- server.c
+++ server.c
@@ -3,9 +3,9 @@
 // date: 4/11/2017
 // Purpose: CS 3376
 // Description: server file containing main method. Continous server that accepts both udp/tcp sockets
-// Continous - implemented, seems to work
-// TCP - implemented, seems to work
-// UDP - Not implemented
+// Continous - implemented, works with 2 clients
+// TCP - implemented, works with 2 clients
+// UDP - implemented, need client.c to test
 // Based very heavily on server2.c and U_server.c from http://www.linuxhowtos.org/C_C++/socket.htm
 // Comments largely based on above article
 // select() used to be able to accept both udp/tcp connections
@@ -20,6 +20,8 @@
 #include <sys/un.h>
 #include <stdio.h>
 #include <iostream>
+#include <string.h>
+#include <netdb.h>
 void error(const char *);
 void dostuff(int sock);
 int main(int argc, char *argv[])
@@ -32,6 +34,7 @@ int main(int argc, char *argv[])
    fd_set readfds; //set containing file descriptors used for reading
 // fd_set writefds; //set containing file descriptors used for writing
 // fd_set exceptfds; //might not end up implementing
+   fd_set master;
    struct timeval tv; //used for select(), sets how long before select times out
    tv.tv_sec = 5; //time to wait in seconds
    tv.tv_usec = 5000000; //time to wait in microseconds - not sure why there are two of these
@@ -52,16 +55,44 @@ int main(int argc, char *argv[])
    if(bind(sockfd,(struct sockaddr *)&serv_addr,servlen)<0) //binds socket to address, needs 3 arguments takes socket file descriptor (sockfd), 
 															//the address it is bound to is(?) cast to the type "sockaddr", and the length of the address
        error("binding socket"); 
-	
+	  
+   //UDP setup
+   int sock_UDP, length_UDP, n;
+   socklen_t fromlen;
+   struct sockaddr_un server;
+   struct sockaddr_un from;
+   char buf_UDP[1024];
 
+   if (argc < 3) {
+      fprintf(stderr, "ERROR, no address provided\n");
+      exit(0);
+   }
+   
+   sock_UDP=socket(AF_UNIX, SOCK_DGRAM, 0);
+   if (sock_UDP < 0) error("Opening socket");
+   length_UDP = sizeof(server);
+   bzero(&server,length_UDP);
+   server.sun_family=AF_UNIX;
+//   server.sun_path=INADDR_ANY;
+   strcpy(serv_addr.sun_path, argv[2]);
+   if (bind(sock_UDP,(struct sockaddr *)&server,length_UDP)<0) 
+       error("binding");
+   fromlen = sizeof(struct sockaddr_un);
+   
+   
+   //main loop, more select setup
    listen(sockfd,5); //listen on socket for connections, second arg is the number of connections that can be waiting while the process handles a connection (backlog list)
-   FD_SET(sockfd, &readfds); //add socket ot readfds, for use by select()
+   FD_SET(sockfd, &readfds); //add socket to readfds, for use by select()
+   FD_SET(sock_UDP, &readfds);
+   master = readfds; //for select()
    clilen = sizeof(cli_addr);
    while(1){
-	   select(1, &readfds, NULL, NULL, &tv); //args: num_fds, fd_set *read_fds, fd_set *write_fds, fd_set *except_fds, struct timeval *timeout
+		readfds = master; //restore readfds to master so select can check again if a connection is waiting
+		select(sock_UDP+1, &readfds, NULL, NULL, &tv); //args: highest fd+1, fd_set *read_fds, fd_set *write_fds, fd_set *except_fds, struct timeval *timeout
 											  //timeval set to null - will never timeout || might only need to use second arg for sockets?
-	   if(FD_ISSET(sockfd, &readfds)); // if TCP connection is detected
-	   {
+		
+		if(FD_ISSET(sockfd, &readfds)) // if TCP connection is detected
+		{
 			newsockfd = accept( //accept causes process to block
 				sockfd,(struct sockaddr *)&cli_addr,&clilen); //arguments: 
 			if (newsockfd < 0) 
@@ -71,13 +102,24 @@ int main(int argc, char *argv[])
 				error("error on fork");
 			if(pid == 0){ //call dostuff(), which will handle all communication once a connection has been established (only processes created by fork() go here)
 				close(sockfd);
-				std::cout << "child reporting for duty\n";
+//				std::cout << "child reporting for duty\n";
 				dostuff(newsockfd);
 				exit(0);		
 			}
-			else
-				close(newsockfd); //close connection with client
-			
+		else
+			close(newsockfd); //close connection with client
+		
+		//UDP
+		if(FD_ISSET(sock_UDP, &readfds)) //if UDP socket can be read
+		{
+			n = recvfrom(sock_UDP,buf_UDP,1024,0,(struct sockaddr *)&from,&fromlen); //read from udp socket
+				if (n < 0) error("recvfrom");
+			write(1,"Received a datagram: ",21); //write to stdout
+			write(1,buf_UDP,n); //write recieved message to stdout
+			n = sendto(sock_UDP,"Got your message\n",17,
+					0,(struct sockaddr *)&from,fromlen); //return a message
+			if (n  < 0) error("sendto");
+		}
 	   }
    }//end of while
    close(sockfd);
